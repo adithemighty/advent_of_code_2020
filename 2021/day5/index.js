947747c0f0c7d897658cf903bdbda9fbea1aab4c
adiyathemighty <ad.kydy@gmail.com>
2021-12-05 12:34:55 +0100

    d5p2

--- 2021/day5/index.js
+++ 2021/day5/index.js
@@ -1,16 +1,38 @@
 const makeRange =
   (noDiagonals = true) =>
   (string) => {
-    const r = string.split(" -> ").map((q) => q.split(",").map(Number));
-    const [[x1, y1], [x2, y2]] = r;
-    if (noDiagonals) {
-      if (x1 !== x2 && y1 !== y2) return [];
-    }
+    const [[x1, y1], [x2, y2]] = string
+      .split(" -> ")
+      .map((q) => q.split(",").map(Number));
     const xEnd = Math.max(x1, x2);
     const xStart = Math.min(x1, x2);
     const yEnd = Math.max(y1, y2);
     const yStart = Math.min(y1, y2);
     const range = [];
+
+    if (x1 !== x2 && y1 !== y2) {
+      if (noDiagonals) return {};
+
+      for (let i = 0; i < xEnd - xStart + 1; i++) {
+        // the direction is either
+        // down+right and up+left
+        // down+right => x1 < x2 && y1 < y2
+        // up+left => x1 > x2 && y1 > y2
+        // OR down+left and up+right
+        // down+left => x1 < x2 && y1 > y2
+        // up+right => x1 > x2 && y1 < y2
+        const direction =
+          (x1 < x2 && y1 < y2) || (x1 > x2 && y1 > y2) ? "down" : "up";
+
+        if (direction === "down") {
+          range.push([xStart + i, yStart + i]);
+        } else {
+          range.push([xStart + i, yEnd - i]);
+        }
+      }
+
+      return { range, xEnd, yEnd };
+    }
 
     for (let x = xStart; x <= xEnd; x++) {
       for (let y = yStart; y <= yEnd; y++) range.push([x, y]);
@@ -25,24 +47,23 @@ class Map {
     this.ranges = [];
     this.noDiagonals = noDiagonals;
     this.vents = [];
+    this.max = 0;
   }
 
-  makeRange = makeRange(this.noDiagonals);
+  makeRange = makeRange;
 
   init() {
+    // figure out length of every row
     let x = 0;
-    let y = 0;
     // handle input
     this.ranges = this.input.split("\n").map((a) => {
-      const { range, xEnd, yEnd } = this.makeRange(a);
+      const { range, xEnd, yEnd } = this.makeRange(this.noDiagonals)(a);
       if (xEnd > x) x = xEnd;
-      if (yEnd > y) y = yEnd;
 
       return range;
     });
 
-    this.maxX = x;
-    this.maxY = y;
+    this.max = x;
   }
 
   setVent(x, y) {
@@ -50,7 +71,7 @@ class Map {
       const curr = this.vents[y][x];
       this.vents[y][x] = curr ? curr + 1 : 1;
     } else {
-      this.vents[y] = new Array(this.maxX + 1);
+      this.vents[y] = new Array(this.max + 1);
       this.vents[y][x] = 1;
     }
   }
--- 2021/day5/index.spec.js
+++ 2021/day5/index.spec.js
@@ -2,12 +2,12 @@ const { Map, makeRange } = require("./");
 const fs = require("fs");
 const path = require("path");
 
-const pathname = path.resolve(__dirname + "/testInput.txt");
+const pathname = path.resolve(__dirname + "/input.txt");
 
 const input = fs.readFileSync(pathname, "utf8", (err, data) => data);
 
 describe("day 5", () => {
-  it("ranges", () => {
+  it("ranges without diagonals", () => {
     const input = "1,1 -> 1,3";
     const input1 = "9,7 -> 7,7";
     const { range: actual } = makeRange()(input);
@@ -23,10 +23,47 @@ describe("day 5", () => {
       [9, 7],
     ];
 
-    // uncomment for testInput
     expect(actual).toEqual(expected);
     expect(actual1).toEqual(expected1);
-    // expect(map.score).toBe(46920);
+  });
+
+  it("ranges with diagonals", () => {
+    const input = "1,1 -> 3,3";
+    const input1 = "3,3 -> 1,1";
+    const input2 = "2,0 -> 6,4";
+    const input3 = "6,4 -> 2,0";
+    const input4 = "9,7 -> 7,9";
+    const input5 = "7,9 -> 9,7";
+    const { range: actual } = makeRange(false)(input);
+    const { range: actual1 } = makeRange(false)(input1);
+    const { range: actual2 } = makeRange(false)(input2);
+    const { range: actual3 } = makeRange(false)(input3);
+    const { range: actual4 } = makeRange(false)(input4);
+    const { range: actual5 } = makeRange(false)(input5);
+    const expected = [
+      [1, 1],
+      [2, 2],
+      [3, 3],
+    ];
+    const expected1 = [
+      [2, 0],
+      [3, 1],
+      [4, 2],
+      [5, 3],
+      [6, 4],
+    ];
+    const expected2 = [
+      [7, 9],
+      [8, 8],
+      [9, 7],
+    ];
+
+    expect(actual).toEqual(expected);
+    expect(actual1).toEqual(expected);
+    expect(actual2).toEqual(expected1);
+    expect(actual3).toEqual(expected1);
+    expect(actual4).toEqual(expected2);
+    expect(actual5).toEqual(expected2);
   });
 
   it("p1", () => {
@@ -36,7 +73,19 @@ describe("day 5", () => {
     const overlap = map.countOverlap();
 
     // uncomment for testInput
-    expect(overlap).toEqual(5);
-    // expect(overlap).toEqual(6666);
+    // expect(overlap).toEqual(5);
+    expect(overlap).toEqual(6666);
+  });
+
+  it("p2", () => {
+    const map = new Map(input, false);
+    map.init();
+    map.mapVents();
+
+    const overlap = map.countOverlap();
+
+    // uncomment for testInput
+    // expect(overlap).toEqual(12);
+    expect(overlap).toEqual(19081);
   });
 });
