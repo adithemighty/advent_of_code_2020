e035422be5016550bdf239791c3a89e05e1e0a41
adiyathemighty <ad.kydy@gmail.com>
2021-12-07 06:37:55 +0100

    d7p2

--- 2021/day7/index.js
+++ 2021/day7/index.js
@@ -1,4 +1,4 @@
-const p1 = (input) => {
+const getMinFuel = (input, p2 = false) => {
   const split = input.split(",").map(Number);
   const max = Math.max(...split);
   const map = split.reduce((acc, val) => {
@@ -7,21 +7,32 @@ const p1 = (input) => {
     return acc;
   }, {});
 
-  const tracker = {};
+  const tracker = {
+    1: {},
+    2: {},
+  };
 
   for (let i = 0; i < max; i++) {
     let distance = 0;
 
-    const thing = Object.keys(map).reduce((acc, a) => {
-      distance += Math.abs(a - i) * map[a];
-
-      acc[i] = distance;
+    Object.keys(map).reduce((acc, a) => {
+      if (p2) {
+        const n = Math.abs(a - i);
+        const triangularNum = (n * (n + 1)) / 2;
+        distance += triangularNum * map[a];
+        acc[2][i] = distance;
+      } else {
+        distance += Math.abs(a - i) * map[a];
+        acc[1][i] = distance;
+      }
 
       return acc;
     }, tracker);
   }
 
-  return Math.min(...Object.values(tracker));
+  return p2
+    ? Math.min(...Object.values(tracker[2]))
+    : Math.min(...Object.values(tracker[1]));
 };
 
-module.exports = { p1 };
+module.exports = { getMinFuel };
--- 2021/day7/index.spec.js
+++ 2021/day7/index.spec.js
@@ -1,15 +1,21 @@
-const { p1 } = require("./");
+const { getMinFuel } = require("./");
 const fs = require("fs");
 const path = require("path");
 
-const pathname = path.resolve(__dirname + "/testInput.txt");
+const pathname = path.resolve(__dirname + "/input.txt");
 
 const input = fs.readFileSync(pathname, "utf8", (err, data) => data);
 
 describe("day 5", () => {
   it("p1", () => {
     // uncomment for test input
-    expect(p1(input)).toBe(37);
-    // expect(p1(input, 80)).toBe(396210);
+    // expect(getMinFuel(input)).toBe(37);
+    expect(getMinFuel(input)).toBe(344297);
+  });
+
+  it("p2", () => {
+    // uncomment for test input
+    // expect(getMinFuel(input, true)).toBe(168);
+    expect(getMinFuel(input, true)).toBe(97164301);
   });
 });
